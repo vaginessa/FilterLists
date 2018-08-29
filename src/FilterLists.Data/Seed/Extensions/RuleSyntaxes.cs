a9db1e1bc5e88d8b9f91aae396d1cdbe6898257a
collinbarrett <collinbarrett@users.noreply.github.com>
2018-08-29 13:04:05 -0500

    convert RuleSyntax seed data to anon types
    ref #202

--- src/FilterLists.Data/Seed/Extensions/RuleSyntaxes.cs
+++ src/FilterLists.Data/Seed/Extensions/RuleSyntaxes.cs
@@ -6,33 +6,45 @@ namespace FilterLists.Data.Seed.Extensions
 {
     public static class RuleSyntaxes
     {
-        private static readonly RuleSyntax[] RuleSyntaxesSeed =
-        {
-            new RuleSyntax
-            {
-                Id = 1, CreatedDateUtc = DateTime.Now, ModifiedDateUtc = DateTime.Now, Name = "Comment"
-            },
-            new RuleSyntax
-            {
-                Id = 2, CreatedDateUtc = DateTime.Now, ModifiedDateUtc = DateTime.Now, Name = "Title"
-            },
-            new RuleSyntax
-            {
-                Id = 3, CreatedDateUtc = DateTime.Now, ModifiedDateUtc = DateTime.Now, Name = "Expires"
-            },
-            new RuleSyntax
-            {
-                Id = 4, CreatedDateUtc = DateTime.Now, ModifiedDateUtc = DateTime.Now, Name = "Platform Hint"
-            },
-            new RuleSyntax
-            {
-                Id = 5, CreatedDateUtc = DateTime.Now, ModifiedDateUtc = DateTime.Now, Name = "Pre-processor Directive"
-            }
-        };
-
         public static void SeedRuleSyntaxes(this ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<RuleSyntax>().HasData(RuleSyntaxesSeed);
+            modelBuilder.Entity<RuleSyntax>()
+                        .HasData(
+                            new
+                            {
+                                Id = 1,
+                                CreatedDateUtc = DateTime.Now,
+                                ModifiedDateUtc = DateTime.Now,
+                                Name = "Comment"
+                            },
+                            new
+                            {
+                                Id = 2,
+                                CreatedDateUtc = DateTime.Now,
+                                ModifiedDateUtc = DateTime.Now,
+                                Name = "Title"
+                            },
+                            new
+                            {
+                                Id = 3,
+                                CreatedDateUtc = DateTime.Now,
+                                ModifiedDateUtc = DateTime.Now,
+                                Name = "Expires"
+                            },
+                            new
+                            {
+                                Id = 4,
+                                CreatedDateUtc = DateTime.Now,
+                                ModifiedDateUtc = DateTime.Now,
+                                Name = "Platform Hint"
+                            },
+                            new
+                            {
+                                Id = 5,
+                                CreatedDateUtc = DateTime.Now,
+                                ModifiedDateUtc = DateTime.Now,
+                                Name = "Pre-processor Directive"
+                            });
         }
     }
 }
