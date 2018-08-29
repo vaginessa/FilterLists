0c51df64d1aa2c6d735d82ee9f6276af59eaeccc
collinbarrett <collinbarrett@users.noreply.github.com>
2018-08-29 14:38:34 -0500

    don't specify seed timestamps

--- src/FilterLists.Data/Seed/Extensions/RuleSyntaxes.cs
+++ src/FilterLists.Data/Seed/Extensions/RuleSyntaxes.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FilterLists.Data.Entities;
+﻿using FilterLists.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace FilterLists.Data.Seed.Extensions
@@ -13,36 +12,26 @@ namespace FilterLists.Data.Seed.Extensions
                             new
                             {
                                 Id = (uint)1,
-                                CreatedDateUtc = DateTime.Now,
-                                ModifiedDateUtc = DateTime.Now,
                                 Name = "Comment"
                             },
                             new
                             {
                                 Id = (uint)2,
-                                CreatedDateUtc = DateTime.Now,
-                                ModifiedDateUtc = DateTime.Now,
                                 Name = "Title"
                             },
                             new
                             {
                                 Id = (uint)3,
-                                CreatedDateUtc = DateTime.Now,
-                                ModifiedDateUtc = DateTime.Now,
                                 Name = "Expires"
                             },
                             new
                             {
                                 Id = (uint)4,
-                                CreatedDateUtc = DateTime.Now,
-                                ModifiedDateUtc = DateTime.Now,
                                 Name = "Platform Hint"
                             },
                             new
                             {
                                 Id = (uint)5,
-                                CreatedDateUtc = DateTime.Now,
-                                ModifiedDateUtc = DateTime.Now,
                                 Name = "Pre-processor Directive"
                             });
         }
