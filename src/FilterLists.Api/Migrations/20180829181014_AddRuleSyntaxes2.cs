e227e333d7be4607e1bb9b612c44b24a5952f670
collinbarrett <collinbarrett@users.noreply.github.com>
2018-08-29 13:11:13 -0500

    add 2nd migration (deletes and re-inserts)
    ref #202

---
+++ src/FilterLists.Api/Migrations/20180829181014_AddRuleSyntaxes2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilterLists.Api.Migrations
+{
+    public partial class AddRuleSyntaxes2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)1);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)2);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)3);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)4);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)5);
+
+            migrationBuilder.InsertData(
+                "rule_syntaxes",
+                new[] {"Id", "CreatedDateUtc", "ModifiedDateUtc", "Name"},
+                new object[,]
+                {
+                    {
+                        (byte)1, new DateTime(2018, 8, 29, 13, 10, 13, 771, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local), "Comment"
+                    },
+                    {
+                        (byte)2, new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local), "Title"
+                    },
+                    {
+                        (byte)3, new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local), "Expires"
+                    },
+                    {
+                        (byte)4, new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local), "Platform Hint"
+                    },
+                    {
+                        (byte)5, new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 10, 13, 775, DateTimeKind.Local), "Pre-processor Directive"
+                    }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)1);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)2);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)3);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)4);
+
+            migrationBuilder.DeleteData(
+                "rule_syntaxes",
+                "Id",
+                (byte)5);
+
+            migrationBuilder.InsertData(
+                "rule_syntaxes",
+                new[] {"Id", "CreatedDateUtc", "ModifiedDateUtc", "Name"},
+                new object[,]
+                {
+                    {
+                        (byte)1, new DateTime(2018, 8, 29, 13, 8, 51, 832, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local), "Comment"
+                    },
+                    {
+                        (byte)2, new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local), "Title"
+                    },
+                    {
+                        (byte)3, new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local), "Expires"
+                    },
+                    {
+                        (byte)4, new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local), "Platform Hint"
+                    },
+                    {
+                        (byte)5, new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local),
+                        new DateTime(2018, 8, 29, 13, 8, 51, 836, DateTimeKind.Local), "Pre-processor Directive"
+                    }
+                });
+        }
+    }
+}
