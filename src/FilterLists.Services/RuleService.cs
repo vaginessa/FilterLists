29734f9c7fc3969a9a8fb4fc5269578ef4ae64b8
Collin Barrett <collinbarrett@users.noreply.github.com>
2018-09-25 05:47:34 -0500

    wip - try precise tagline rule count
    ref #390

--- src/FilterLists.Services/RuleService.cs
+++ src/FilterLists.Services/RuleService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using FilterLists.Data;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,14 @@ namespace FilterLists.Services
         }
 
         public async Task<int> GetCountAll() =>
-            await DbContext.Rules.AsNoTracking().CountAsync();
+            await DbContext.Snapshots
+                           .AsNoTracking()
+                           .Where(s => s.WasUpdated && s.WasSuccessful)
+                           .OrderByDescending(s => s.CreatedDateUtc)
+                           .GroupBy(s => s.FilterListId)
+                           .Select(g => g.FirstOrDefault())
+                           .SelectMany(s => s.SnapshotRules)
+                           .GroupBy(sr => sr.RuleId)
+                           .CountAsync();
     }
 }
